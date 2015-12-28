48bd4a3b08f77b9e86b9001f8fd97f09a1cbcb0f
tragatron <ryan.traeger16@gmail.com>
2015-12-27 22:17:21 -0600

    set up of html pages and mongoose

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 express     = require('express'),
 app         = express(),
 bodyparser  = require('body-parser'),
+mongoose    = require('mongoose'),
 mongodb     = require('mongodb'),
 port        = 8000,
 root        = __dirname;
@@ -11,13 +12,20 @@ app.use(bodyparser.json());
 app.use('/node_modules', express.static(root + '/client/node_modules'));
 app.use('/contorllers', express.static(root + '/client/controllers'));
 app.use('/js', express.static(root + '/client/js'));
-app.use('/templates', express.static(root + '/client/templates'));
 app.use('/css', express.static(root + '/client/css'));
 app.use('/images', express.static(root + '/client/images'));
 
 //Routes:
 app.get('/', function(req, res){
   res.sendFile(root + '/client/views/index.html');
+});
+
+app.get('/randomizer', function(req, res){
+  res.sendFile(root + '/client/views/randomizer.html');
+});
+
+app.get('/about', function(req, res){
+  res.sendFile(root + '/client/views/about.html');
 });
 
 //Server Setup:
